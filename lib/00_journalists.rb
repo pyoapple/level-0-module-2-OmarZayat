62ef7e965d8393328f268e0e36797093079f3b02
pyoapple <112991292+pyoapple@users.noreply.github.com>
2020-02-27 07:12:01 +0000

    Updated test file

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -1,3 +1,3 @@
 
 def liste
-	["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@Cl
+	["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@steph
