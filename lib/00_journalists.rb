ce0667c7afd32bed1b34793664ba3b0bfed1e5bb
pyoapple <112991292+pyoapple@users.noreply.github.com>
2020-03-13 18:42:53 +0000

    o filtering version in doc

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -1,3 +1,3 @@
 
 def liste
-	["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@Nolwen
+	["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson
