f8cd7e187cac2383ad4a1475dd162bec7d0d3325
pyoapple <112991292+pyoapple@users.noreply.github.com>
2022-08-13 13:42:15 +0000

    ALSA: ua101: Fix unlocked snd_pcm_stop() call

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -26,4 +26,4 @@ def nombreHandles(nH)
 	puts "\n"
 	puts "2- Combien d'handle contiennent un numéro ?"
 	puts "\n"
-	puts "=====> Il y en a #{number_c
+	puts "=====> Il y en a #{number_count} qui cont
