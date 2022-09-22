c8b5c796193dd10b20f8d3882254819aed1c7f75
pyoapple <112991292+pyoapple@users.noreply.github.com>
2022-09-22 11:32:36 +0000

    package

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -43,4 +43,4 @@ def cnombreAude(nAude)
   	end
 	puts "3- Combien d'handle contiennent les 4 lettres du prénom \"Aude\" à la suite (sans faire attention à la casse) ?"
 	puts "\n"
-	puts "
+	puts "=====> Il 
