9839e0a2af3c227b63cf8d900916be7e10837b4b
pyoapple <112991292+pyoapple@users.noreply.github.com>
2022-12-18 12:37:58 +0000

    fix docstring typo

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -94,4 +94,4 @@ def nombreUnderscore(nU)
   	end
   	puts "6- Combien d'underscore dans tous les pseudos confondus ?"
   	puts "\n"
-  	puts "=====> Il y en a #{nbr} und
+  	puts "=====> Il y en a #{nbr} underscore"
