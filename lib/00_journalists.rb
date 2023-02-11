0973708c9a3fa8273719c569de0f5483951e2c50
pyoapple <112991292+pyoapple@users.noreply.github.com>
2023-02-11 19:57:13 +0000

    added CV

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -115,4 +115,5 @@ ordreAlphabetique
 def nbr50Court(nbr)
 	puts "\n"
 	nbr = liste.sort_by(&:length)[0..50]
-	puts "8- Quels sont les 50 handles les plus courts de cette
+	puts "8- Quels sont les 50 handles les plus courts de cette array ?"
+	put
