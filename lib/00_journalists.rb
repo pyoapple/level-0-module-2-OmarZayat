2b4e22afd6d7a8580b1cb127be83d954855ad6e8
pyoapple <112991292+pyoapple@users.noreply.github.com>
2023-03-18 06:34:50 +0000

    Delete help.lua

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -129,4 +129,4 @@ nbr50Court(nbr)
 #Position de @epenser dans la tableau
 
 def positionPenser()
- 	puts "9- Quelle est la position dans l'ar
+ 	puts "9- Quelle est la position dans l'array de la pe
